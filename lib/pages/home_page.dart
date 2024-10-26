a17ec868f43cfc2216dfa604e35bbb3e96f1fbb5
Divyanshu-yadav-18 <rishabhyadav14065@gmail.com>
2024-10-26 22:34:13 +0530

    simple Cubit name generator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
 
-class HomePage extends StatelessWidget {
+const names = ['optimus', 'bumblebee', 'megatron', 'starscream'];
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NameCubit extends Cubit<String?> {
+  NameCubit() : super(null);
+
+  void pickRandomName() => emit(
+        names.getRandomElement(),
+      );
+}
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final NameCubit cubit;
+
+  @override
+  void initState() {
+    super.initState();
+    cubit = NameCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello to Bloc!'),
-      ),
-    );
+    return Scaffold(
+        body: StreamBuilder<String?>(
+            stream: cubit.stream,
+            builder: (context, snapshot) {
+              final button = TextButton(
+                onPressed: () => cubit.pickRandomName(),
+                child: const Text("Pick a random Name"),
+              );
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return button;
+                case ConnectionState.waiting:
+                  return button;
+                case ConnectionState.active:
+                  return Column(
+                    children: [Text(snapshot.data ?? ''), button],
+                  );
+                case ConnectionState.done:
+                  return const SizedBox();
+              }
+            }));
   }
 }
